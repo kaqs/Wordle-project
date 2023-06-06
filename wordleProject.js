8c3b3e8e7f95f26633192302a898122f63e9e37f
karine <kaqsanfelice@gmail.com>
2023-06-06 17:57:28 +0100

    day 2 - created loop to show index of guess and secretWord

--- wordleProject.js
+++ wordleProject.js
@@ -1,28 +1,20 @@
-let guess;
+let guess = "rates";
 let secretWord = "water";
 let attempts = 0; // wordle game has 6 attempts
 
-while (guess != secretWord && attempts < 6) {
-    attempts++;
-    guess = prompt(`Please enter a word containing 5 letters`);
-    if (guess == secretWord) {
-        alert(`Amazing! You win!`);
-    } else {
-        alert(`"Incorrect! You still have ${6-attempts}"`);
+for (let indexSecret = 0; indexSecret < secretWord.length; indexSecret++) {
+
+    for (let indexGuess = 0; indexGuess < guess.length; indexGuess++) {
+        console.log(`(${indexSecret}, ${indexGuess})`);
+        console.log(`(${secretWord[indexSecret]}, ${guess[indexGuess]})`);
+        // console.log(`valor de indexSecret: ${indexSecret},`);
+        // console.log(`valor de secretWord no index de indexSecret (secretWord[indexSecret]): ${secretWord[indexSecret]},`);
+
+        // console.log(`valor de indexGuess: ${indexGuess},`);
+        // console.log(`valor de guess no index de indexGuess (guess[indexGuess]): ${guess[indexGuess]},`);
+
     }
+
 }
 
-if (guess != secretWord) {
-    alert(`Unlucky! The word was ${secretWord}`);
-}
 
-// approach using FOR statement
-// for (let attempts = 0; attempts < 3; attempts++) {
-//     guess = prompt(`"Please enter a number between ${min} and ${max}"`);
-//     if (guess == secretNumber) {
-//         alert(`"We have a winner! Congratulations, you got it right after ${attempts+1}"`);
-//         break;
-//     } else {
-//         alert (`"Incorrect! You still have ${2-attempts}"`);
-//     }
-// } 
