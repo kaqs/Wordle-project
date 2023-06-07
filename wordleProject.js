4d34f1e761052a6b74b5d99d5131ee9313b7528b
karine <kaqsanfelice@gmail.com>
2023-06-07 13:54:54 +0100

    day 3 - created loop to compare index of guess and secretWord

--- wordleProject.js
+++ wordleProject.js
@@ -1,20 +1,39 @@
 let guess = "rates";
 let secretWord = "water";
 let attempts = 0; // wordle game has 6 attempts
+let indexGuess;
+let indexSecret;
+let responseList = [];
 
-for (let indexSecret = 0; indexSecret < secretWord.length; indexSecret++) {
+for (indexGuess = 0; indexGuess < guess.length; indexGuess++) {
+    let response = `"${guess[indexGuess]} ✗"`;
 
-    for (let indexGuess = 0; indexGuess < guess.length; indexGuess++) {
-        console.log(`(${indexSecret}, ${indexGuess})`);
-        console.log(`(${secretWord[indexSecret]}, ${guess[indexGuess]})`);
-        // console.log(`valor de indexSecret: ${indexSecret},`);
+    for (indexSecret = 0; indexSecret < secretWord.length; indexSecret++) {
+
+        //console.log(guess.includes(secretWord[indexSecret]));
+
+        if (guess[indexGuess] === secretWord[indexSecret] && indexGuess === indexSecret) {
+            response = `"${guess[indexGuess]} ✓"`;
+
+            //console.log(`(${indexSecret}, ${indexGuess})`);
+            //console.log(`(${secretWord[indexSecret]}, ${guess[indexGuess]})`);
+        } else if (guess[indexGuess] === secretWord[indexSecret] && indexGuess !== indexSecret) {
+            response = `"${guess[indexGuess]} ⍻"`;
+
+            //console.log(`(${indexSecret}, ${indexGuess})`);
+            //console.log(`(${secretWord[indexSecret]}, ${guess[indexGuess]})`);
+        }
+        // // console.log(`valor de indexSecret: ${indexSecret},`);
         // console.log(`valor de secretWord no index de indexSecret (secretWord[indexSecret]): ${secretWord[indexSecret]},`);
 
         // console.log(`valor de indexGuess: ${indexGuess},`);
         // console.log(`valor de guess no index de indexGuess (guess[indexGuess]): ${guess[indexGuess]},`);
 
     }
-
+    responseList.push(response);
 }
+console.log(responseList);
+
+
 
 
