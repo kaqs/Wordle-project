59c78ff775bc0c842d64c4e71e569fe72c4e06e5
karine <kaqsanfelice@gmail.com>
2023-06-12 10:19:37 +0100

    day 4 - initial html, css and js

---
+++ wordle.js
@@ -0,0 +1,11 @@
+function submitGuess() {
+    alert("guess retrieved");
+}
+
+function changeColour() {
+}
+
+
+document.getElementById("submitbutton").onclick = submitGuess;
+
+document.getElementById("testbutton").onclick = changeColour;
