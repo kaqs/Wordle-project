581fad9e2c5def73fcbf2df5507317f9ec1e81be
karine <kaqsanfelice@gmail.com>
2023-06-14 11:49:30 +0100

    day 7 - fixed issue of a single char compared many times, and priority issue by adding a 2nd loop

--- wordle.js
+++ wordle.js
@@ -10,30 +10,45 @@ let message = document.getElementById("messageendgame");
 
 function submitGuess() {
 
+    // new variable created to be used/adjusted as necessary without affecting the original variable
+    let internalSecretWord = secretWord;
+
     guess = document.getElementById("guess").value;
 
     const inputElement = document.getElementById("guess");
     const inputElementvalue = inputElement.value;
 
+    // 1st loop: it assumes all the letters does not match and are not included in internalSecretWord (grey colour)
     for (indexGuess = 0; indexGuess < 5; indexGuess++) {
         const id = attempts.toString() + indexGuess.toString();
         const boxElement = document.getElementById(id);
         let guessBoxText = inputElementvalue[indexGuess].toUpperCase();
         boxElement.innerText = guessBoxText;
+        boxElement.style.background = "#a6aba8";
 
+        // this FOR statement loops through internalSecretWord and then compares the character and index of guess
+        for (indexSecret = 0; indexSecret < internalSecretWord.length; indexSecret++) {
 
-        for (indexSecret = 0; indexSecret < secretWord.length; indexSecret++) {
-
-            if (guess[indexGuess] === secretWord[indexSecret] && indexGuess === indexSecret) {
+            // this IF statement identifies all characters that matches the position of internalSecretWord characters and their index (green colour)
+            if (guess[indexGuess] === internalSecretWord[indexSecret] && indexGuess === indexSecret) {
                 boxElement.style.background = "#2cb851";
-                break;
+                // the substring function returns a new string with the matched characters replaced by a *, preventing the original matched character being compared again.
+                internalSecretWord = internalSecretWord.substring(0, indexSecret) + "*" + internalSecretWord.substring(indexSecret + 1, indexSecret.lenght);
+            }
+        }
+    }
 
-            } else if (guess[indexGuess] === secretWord[indexSecret] && indexGuess !== indexSecret) {
+    // 2nd loop: it takes the outcome from first loop to compare if the remaining characters are included in internalSecretWord (yellow colour)
+    for (indexGuess = 0; indexGuess < 5; indexGuess++) {
+        const id = attempts.toString() + indexGuess.toString();
+        const boxElement = document.getElementById(id);
+
+        for (indexSecret = 0; indexSecret < internalSecretWord.length; indexSecret++) {
+
+            if (guess[indexGuess] === internalSecretWord[indexSecret] && indexGuess !== indexSecret) {
                 boxElement.style.background = "#dfe615";
-                break;
-
-            } else if (guess[indexGuess] !== secretWord[indexSecret] && indexGuess !== indexSecret) {
-                boxElement.style.background = "#a6aba8";
+                // the substring function returns a new string with the matched characters replaced by a *, preventing the original matched character being compared again.
+                internalSecretWord = internalSecretWord.substring(0, indexSecret) + "*" + internalSecretWord.substring(indexSecret + 1, indexSecret.lenght);
             }
         }
     }
@@ -42,10 +57,7 @@ function submitGuess() {
         message.innerText = "Perfect! You win!";
     }
     attempts++
-
 }
-
-
 submit.onclick = submitGuess;
 
 
